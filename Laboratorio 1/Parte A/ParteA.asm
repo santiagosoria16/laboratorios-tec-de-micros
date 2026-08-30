c14152aad398afe2288b8f2df2b0f24a43d6f6dc
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-08-30 18:37:09 -0300

    Add definitions and setup for ParteA.asm

--- Laboratorio 1/Parte A/ParteA.asm
+++ Laboratorio 1/Parte A/ParteA.asm
@@ -1,1 +1,16 @@
+.include "m328pdef.inc"
+
+.def temp     = r16
+.def estado   = r17
+.def carga    = r18    
+.def timer_cnt= r19
+
+.equ E_STANDBY     
+.equ E_LAVADO      
+.equ E_CENTRIFUGADO
+.equ E_SECADO      
+.equ E_FIN         
+
+.org 0x0000
+ rjmp SETUP
 
