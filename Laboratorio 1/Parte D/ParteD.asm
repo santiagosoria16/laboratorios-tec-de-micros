af872c984347ed5b13f883ddd677c593ba293c5c
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-08-31 18:29:03 -0300

    Define registers and flags in ParteD.asm

--- Laboratorio 1/Parte D/ParteD.asm
+++ Laboratorio 1/Parte D/ParteD.asm
@@ -1,1 +1,12 @@
+.include "m328pdef.inc"
 
+.def R_A      = r16  
+.def R_B      = r17 
+.def R_F      = r18  
+.def FLAG_C   = r19 
+.def FLAG_N   = r20 
+.def FLAG_Z   = r21 
+.def R_SEL    = r22  
+.def R_OUT    = r23  
+.def R_TEMP1  = r24  
+.def R_TEMP2  = r25 
