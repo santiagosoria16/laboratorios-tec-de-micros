2afa892fb2be29f15548be0b4b3a604cc74abfd9
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-09-25 20:25:28 -0300

    Update ParteE.asm

--- laboratorio 2 /Codigos/ParteE.asm
+++ laboratorio 2 /Codigos/ParteE.asm
@@ -1,1 +1,14 @@
+.include "m328pdef.inc"
+
+.def STATE        = r20   
+.def OBS_FLAG     = r22  
+.def TEMP         = r16
+.def TEMP2        = r17
+
+.equ ST_CERRADA      = 0
+.equ ST_ABRIENDO     = 1
+.equ ST_ABIERTA      = 2
+.equ ST_CERRANDO     = 3
+.equ ST_INT_ABRIENDO = 4
+.equ ST_INT_CERRANDO = 5
 
