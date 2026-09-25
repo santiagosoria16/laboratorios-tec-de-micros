76d7305b53562018899a4daabd545e323667c762
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-09-25 20:27:57 -0300

    Initialize ports and USART settings in ParteE.asm

--- laboratorio 2 /Codigos/ParteE.asm
+++ laboratorio 2 /Codigos/ParteE.asm
@@ -26,3 +26,28 @@ RESET:
 
     cbi DDRB, 0
     sbi PORTB, 0
+
+    clr TEMP
+    out DDRC, TEMP
+    ldi TEMP, 0x0F     
+    out PORTC, TEMP
+
+    ldi TEMP, (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD1)
+    out DDRD, TEMP
+    clr TEMP
+    out PORTD, TEMP   
+
+ 
+    ldi TEMP, (1<<PCIE0)
+    sts PCICR, TEMP      
+    ldi TEMP, (1<<PCINT0)
+    sts PCMSK0, TEMP    
+
+    ldi TEMP, 0
+    sts UBRR0H, TEMP
+    ldi TEMP, 103
+    sts UBRR0L, TEMP
+    ldi TEMP, (1<<TXEN0)
+    sts UCSR0B, TEMP     
+    ldi TEMP, (1<<UCSZ01) | (1<<UCSZ00)
+    sts UCSR0C, TEMP   
