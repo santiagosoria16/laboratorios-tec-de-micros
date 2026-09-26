17cec444f61a3de3afc306ed9fec6ff056675860
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-09-26 15:06:03 -0300

    Initialize stack pointer and configure PORTD

--- laboratorio 2 /Codigos/ParteA.asm
+++ laboratorio 2 /Codigos/ParteA.asm
@@ -1,1 +1,19 @@
+.include "m328pdef.inc"
+
+.equ FRAME_BUF = 0x0100
+
+.org 0x0000
+    rjmp RESET
+
+RESET:
+
+    ldi r16, LOW(RAMEND)
+    out SPL, r16
+    ldi r16, HIGH(RAMEND)
+    out SPH, r16
+
+    ldi r16, 0xFE        
+    out DDRD, r16
+    ldi r16, 0xFC        
+    out PORTD, r16
 
