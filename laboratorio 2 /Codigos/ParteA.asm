438fca7c7f69f1dbaeaa3081627e7856cbb4020b
Scania V8 770 <santiago.soria@estudiantes.utec.edu.uy>
2026-09-26 15:06:43 -0300

    Add UART initialization and RAM clearing routine
    Initialize UART and clear FRAME_BUF in RAM.

--- laboratorio 2 /Codigos/ParteA.asm
+++ laboratorio 2 /Codigos/ParteA.asm
@@ -17,3 +17,24 @@ RESET:
     ldi r16, 0xFC        
     out PORTD, r16
 
+    ldi r16, 0x3F        
+    out DDRB, r16
+    ldi r16, 0x03       
+    out PORTB, r16
+
+
+    ldi r16, 0x0F     
+    out DDRC, r16
+    clr r16
+    out PORTC, r16     
+
+    rcall INIT_UART
+
+    ldi YL, LOW(FRAME_BUF)
+    ldi YH, HIGH(FRAME_BUF)
+    clr r16
+    ldi r17, 8
+CLR_RAM:
+    st Y+, r16
+    dec r17
+    brne CLR_RAM
